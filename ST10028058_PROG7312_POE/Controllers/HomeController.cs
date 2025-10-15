24a5f477eddc011fb9f67013b308418c7b6dd375
ST10028058-Sashiel <128804235+ST10028058-Sashiel@users.noreply.github.com>
2025-10-15 15:29:52 +0200

    updated layout

--- ST10028058_PROG7312_POE/Controllers/HomeController.cs
+++ ST10028058_PROG7312_POE/Controllers/HomeController.cs
@@ -30,3 +30,18 @@ namespace ST10028058_PROG7312_POE.Controllers
         }
     }
 }
+//# Assistance provided by ChatGPT
+//# Code and support generated with the help of OpenAI's ChatGPT.
+// code attribution
+// W3schools
+//https://www.w3schools.com/cs/index.php
+
+// code attribution
+//Bootswatch
+//https://bootswatch.com/
+
+// code attribution
+// https://learn.microsoft.com/en-us/aspnet/core/tutorials/first-mvc-app/start-mvc?view=aspnetcore-8.0&tabs=visual-studio
+
+// code attribution
+// https://learn.microsoft.com/en-us/aspnet/core/security/authentication/identity?view=aspnetcore-8.0&tabs=visual-studio
